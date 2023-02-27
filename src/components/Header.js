2e4fad5d37cd0d3cc3962d523c1eeae450a22b88
Hà Trường <havantruong@vnpt.vn>
2023-02-27 08:04:26 +0700

    add routing

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,21 @@
 import React from "react";
+import {NavLink} from 'react-router-dom'
+
+const activeClass = (params) => {
+	return params.isActive ? "active-item" : "";
+}
 
 export default function Header() {
     return (
         <div className='header-container'>
             <ul>
                 <li style={{float:'left'}}><a href="#MEN">MEN</a></li>
-                <li><a href="#About">About</a></li>
-                <li><a href="#Sale">Sale</a></li>
-                <li><a href="#Assersory">Assersory</a></li>
-                <li><a href="#Clothing">Clothing</a></li>
+                <li>
+                    <NavLink to="/admin" className={activeClass}>Admin</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/home" className={activeClass}>Home</NavLink>
+                </li>
             </ul>
         </div>
     )
